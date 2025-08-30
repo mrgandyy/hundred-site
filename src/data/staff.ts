0aee4be9153342f4a70d74b4de5b9515752f995d
twerktaco <itstwerktaco@gmail.com>
2025-08-29 18:37:11 -0600

    fix3

--- src/data/staff.ts
+++ src/data/staff.ts
@@ -5,6 +5,6 @@ export const staff: StaffItem[] = [
 { name: "Jun-Hee", role: "Admin", bio: "When chaos hits 99, Jun-Hee turns it back to 100—clean chats, smooth queues, safe parties.", avatar: "https://api.dicebear.com/9.x/identicon/svg?seed=JUNE" },
 { name: "SMOKE", role: "Admin", bio: "SMOKE keeps the vibes high and the servers calm—fast, fair, and always on watch.", avatar: "https://api.dicebear.com/9.x/identicon/svg?seed=SMOKE" },
 { name: "STACKS", role: "Host", bio: "Known for keeping the crowd engaged and the momentum alive, he’s not just a host — he’s the hype", avatar: "https://api.dicebear.com/9.x/identicon/svg?seed=Stacks" },
-{ name: "ChickenTTV", role: "Admin", bio: "The Hundreds #1 Announcer", avatar: "https://api.dicebear.com/9.x/identicon/svg?seed=Chicken" },
-{ name: "AlexCorruptor", role: "Admin", bio: "Seemingly quiet person with massive potential. Enjoys keeping things civilised.", avatar: "https://api.dicebear.com/9.x/identicon/svg?seed=alex" },
+{ name: "ChickenTTV", role: "Staff", bio: "The Hundreds #1 Announcer", avatar: "https://api.dicebear.com/9.x/identicon/svg?seed=Chicken" },
+{ name: "AlexCorruptor", role: "Staff", bio: "Seemingly quiet person with massive potential. Enjoys keeping things civilised.", avatar: "https://api.dicebear.com/9.x/identicon/svg?seed=alex" },
 ];
